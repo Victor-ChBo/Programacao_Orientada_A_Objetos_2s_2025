7ff8642ea3188269302f0e30c3f95ba8e847b26e
Victor-ChBo <victor.bolanos@a.ucb.br>
2025-10-12 15:04:30 -0300

    atividade2

---
+++ atividade2/Revista.java
@@ -0,0 +1,20 @@
+package atividade2;
+
+public class Revista extends Material { 
+    
+    private int edicao; 
+
+    public Revista(String titulo, Integer anoPublicacao, int edicao){
+        super(titulo, anoPublicacao); 
+        this.edicao = edicao;
+    }
+
+    @Override
+    public void descricao() {
+        System.out.println("Tipo: Revista");
+        System.out.println("Título: " + getTitulo());
+        System.out.println("Ano de Publicação: " + getAnoPublicacao());
+        System.out.println("Edição: " + this.edicao);
+    }
+
+}
